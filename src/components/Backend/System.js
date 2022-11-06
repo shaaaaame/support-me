3292a00597cd610bc4c9e4c46f6daaf89903a49d
Andy Liu <andyliu200488@gmail.com>
2022-11-06 06:11:06 -0500

    sixth commit

--- src/components/Backend/System.js
+++ src/components/Backend/System.js
@@ -1,16 +1,26 @@
-import {userSignIn, userSignUp, isExistUser, getUserNum, findUsers, findUserById, findUsersByTags, 
-    sortTutorByRating, updateUser, getCourseNum, findCourseById, findCoursesByUserId, updateCourse, 
-    postTutorOder, getTutorOrder, createTutorRequest, sendTutorRequest, acceptCourse, rejectCourse, 
-    finishedCourse, rating} from './Database.js'
+import {userSignUp, isExistUser, getTutorOrder, findUsersByTags, sortTutorByRating} from './Database.js'
 
 
 class System {
-    static async regsiter(email, password, phone_number, first_name, last_name, age, education_level, introduction, tutor_tags, student_tags) {
-        if (!await isExistUser('email', email)) return await userSignUp([first_name, last_name, age, education_level, tutor_tags, student_tags, email, phone_number, password, introduction]);
+    static async regsiter(email, password, phone_number, first_name, last_name, age, education_level, introduction, 
+        tutor_tags, student_tags) {
+        if (!await isExistUser('email', email)) return await userSignUp([first_name, last_name, age, education_level, 
+            tutor_tags, student_tags, email, phone_number, password, introduction]);
         return false;
     }
 
     static async showQuestions() {
         return await getTutorOrder();
     }
+
+    static async showTutors(keyword) {
+        return await findUsersByTags('tutor', keyword);
+    }
+
+    static async showRank() {
+        return await sortTutorByRating();
+    }
 }
+
+
+export {System};
