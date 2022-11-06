89da3b378a1835407cf93343d4ccc4e31752b998
Han Xheng Chew <81433191+shaaaaame@users.noreply.github.com>
2022-11-06 06:34:17 -0500

    added showquestions

--- src/components/Question/QuestionAsked/QuestionAsked.jsx
+++ src/components/Question/QuestionAsked/QuestionAsked.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import QuestionCard from '../QuestionCard/QuestionCard'
-import './QuestionAsked.css'
+import './QuestionAsked.css';
+import {System} from '../../Backend/System';
+
+let questions = await System.showQuestions();
 
 function QuestionAsked() {
   return (
@@ -8,9 +11,9 @@ function QuestionAsked() {
         <h1 className='question-asked-title'>help is on their way.</h1>
         <h2 className='question-asked-subtitle'>while we're waiting, can you help <b>them?</b></h2>
         <div className='question-asked-cards'>
-            <QuestionCard title="adsadsdadadsa" />
-            <QuestionCard title="adsadsdadadsa"/>
-            <QuestionCard title="adsadsdadadsa"/>
+            <QuestionCard title={questions[0]['description']} />
+            <QuestionCard title={questions[1]['description']}/>
+            <QuestionCard title={questions[2]['description']}/>
         </div>
     </div>
   )
